8c469c3a34fa70c4a8c5f50f5436fa4ef6b97b65
Chris Joy <chrisjoydev@gmail.com>
2017-08-06 22:53:18 +1000

    echon.sh fix

--- labs/lab03/echon.sh
+++ labs/lab03/echon.sh
@@ -4,11 +4,11 @@ num=$(($1))
 word="$2"
 if (($argc != 2)) || (($num < 0))  ; then
 	echo "Usage: ./echon.sh <number of lines> <string>"
+else
+	x=0
+	while (($x < $num))
+	do
+		echo $2
+		x=$(($x+1))
+	done
 fi
-
-x=0
-while (($x < $num))
-do
-	echo $2
-	x=$(($x+1))
-done
